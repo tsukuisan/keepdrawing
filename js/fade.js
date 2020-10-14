174c8c521a1a2a132564312963c47ac4b7b657be
津久井千佳 <chika@tsukuichikanoMacBook-Pro.local>
2020-10-14 23:53:17 +0900

    フェードイン完了！！

--- js/fade.js
+++ js/fade.js
@@ -1,38 +1,13 @@
 $(function () {
     $(window).scroll(function () {
-        const wHeight = $(window).height();
+        const wHeight = screen.height;
         const scrollAmount = $(window).scrollTop();
         $('.scrollanime').each(function () {
             const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 1) {
+            if(scrollAmount > targetPosition - wHeight + 500) {
                 $(this).addClass("fadeInDown");
             }
         });
     });
 });
 
-$(function () {
-    $(window).scroll(function () {
-        const wHeight = $(window).height();
-        const scrollAmount = $(window).scrollTop();
-        $('.scrollanime1').each(function () {
-            const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 1) {
-                $(this).addClass("fadeInDown");
-            }
-        });
-    });
-});
-
-$(function () {
-    $(window).scroll(function () {
-        const wHeight = $(window).height();
-        const scrollAmount = $(window).scrollTop();
-        $('.scrollanime2').each(function () {
-            const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 1) {
-                $(this).addClass("fadeInDown");
-            }
-        });
-    });
-});
